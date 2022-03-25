4f4ba6fed34d03f3d8aab7ec853cd30d904dacab
WaldoB1337 <62052923+WaldoB1337@users.noreply.github.com>
2022-03-25 16:41:16 +0100

    Update BAL_Lift_Correct.m

--- BALANCE AND PRESSURE DATA/BAL_Lift_Correct.m
+++ BALANCE AND PRESSURE DATA/BAL_Lift_Correct.m
@@ -4,6 +4,6 @@ function [d_C_D_W] = BAL_Lift_Correct(C_L_W)
 S = 0.21272 ;% [m2]; Wing Area
 C = (1.80 * 1.25) - (4*0.5*0.3^2);% [m2]
 %% Input Data
-delta = 5; % Boundary Correction Factor
+delta = 0.104; % Boundary Correction Factor
 d_C_D_W = delta* (S/C) * C_L_W;
 end
