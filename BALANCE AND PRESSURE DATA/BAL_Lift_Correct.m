897619c7be2f293812c40a4bd17c82b722e9b57f
mvretoudakis <e.vretoudakis@student.tudelft.nl>
2022-03-31 14:00:09 +0200

    Lift cor

--- BALANCE AND PRESSURE DATA/BAL_Lift_Correct.m
+++ BALANCE AND PRESSURE DATA/BAL_Lift_Correct.m
@@ -18,5 +18,14 @@ k = b_e/B; % Effective span/Jet width
 % we get delta from the graph [Barlow, Rae, and Pope, 1999]
 
 delta = 0.104; % Boundary Correction Factor
+
+%% Import C_L_w for alpha=0 and =5 and linearly interpolate for alpha=4
+C_L_Wa0 = [0.323, 0.3008, 0.2982, 0.2963, 0.3192];
+C_L_Wa5 = [0.7632, 0.7448, 0.7413, 0.7410, 0.7622];
+
+% Interpolated C_L value for a=4 and b=-10,-5,0,5,10
+C_L_Wa4 = C_L_Wa0 + 4/5*(C_L_Wa5 - C_L_Wa0);
+
+
 d_C_D_W = delta* (S/C) * C_L_W;
 end
